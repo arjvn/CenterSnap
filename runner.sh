9e9d67a2e3f2fa76e56a93d6c6f21dc25f66af58
Arjvn <arjunkozik@gmail.com>
2024-05-20 00:31:25 +0100

    init

---
+++ runner.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -Eeuo pipefail
+
+SCRIPT_DIR=$(dirname $(readlink -f $0))
+echo "Running from $SCRIPT_DIR"
+export PYTHONPATH=$(readlink -f "${SCRIPT_DIR}")
+export PYTHONPATH=$PYTHONPATH:$(readlink -f "${SCRIPT_DIR}/export")
+export OPENBLAS_NUM_THREADS=1
+export PYOPENGL_PLATFORM=egl
+
+# $SCRIPT_DIR/env/bin/python $@
